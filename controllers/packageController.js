40b247ce4e9a2c22aaed0f2b3d01a77d578c03eb
Faizal Ismail <muhammad.faizal@finexusgroup.com>
2024-12-07 14:52:11 +0800

    move processvip on top

--- controllers/packageController.js
+++ controllers/packageController.js
@@ -22,6 +22,70 @@ const getPackage = asyncHandler(async (req, res) => {
     }
 });
 
+const processVIPCommision = async (member, amount) => {
+    console.log(`Processing VIP Referral Commision`);
+
+    // Percentages for each level
+    const percentages = [20, 2, 2, 2, 1.2, 1.2, 0.8, 0.8, 0.4, 0.4, 0.2, 0.2, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1];
+    console.log(percentages.length);
+
+    let currentMember = member.referredBy;
+    let level = 0;
+
+    const visited = new Set();
+
+    try {
+        while (currentMember && level < 20) {
+            if (visited.has(currentMember)) {
+                console.log('Cycle detected in referral chain. Breaking loop.');
+                break;
+            }
+            visited.add(currentMember);
+
+            // Find the upline member
+            const uplineMember = await Member.findOne({referralCode: currentMember}).select('_id fullName referralCode referredBy type');
+            if (!uplineMember)
+                break;
+
+            // Calculate the commission for this level
+            const percentage = percentages[level] ?? 0; // If percentage not specied, then 0 commission
+            const commission = (amount * percentage) / 100;
+
+
+            if (uplineMember.type !== 'VIP' && level < 3) {
+                console.log(`Upline Member ${uplineMember.fullName} (Level ${level + 1}) missed on receiving ${percentage}% (RM ${(commission / 100).toFixed(2)})`);
+            } else {
+                const uplineWallet = await Wallet.findOne({memberId: uplineMember._id}).select('balance');
+                if (!uplineWallet) {
+                    console.log(`Wallet not found for upline member ${uplineMember.fullName}`);
+                } else {
+                    uplineWallet.balance = Number(uplineWallet.balance) + commission;
+                    await uplineWallet.save();
+
+                    await Transaction.create({
+                        walletId: uplineWallet._id,
+                        systemType: 'HubWallet',
+                        type: 'Credit',
+                        description: 'VIP Registration Commission',
+                        status: 'Success',
+                        memberId: member._id,
+                        amount: amount
+                    });
+
+                    console.log(`Upline Member ${uplineMember.fullName} (Level ${level + 1}) received ${percentage}% (RM ${(commission / 100).toFixed(2)})`);
+                }
+            }
+
+            // Move to the next upline
+            currentMember = uplineMember.referredBy;
+            level++;
+        }
+    } catch (error) {
+        console.error(`Error processing VIP commission at Level ${level + 1}: ${error.message}`);
+        console.error(error.stack);
+    }
+};
+
 const purchasePackage = asyncHandler(async (req, res) => {
     const {code, paymentChannel} = req.body;
 
@@ -138,70 +202,5 @@ const purchasePackage = asyncHandler(async (req, res) => {
     }
 
 });
-
-
-const processVIPCommision = async (member, amount) => {
-    console.log(`Processing VIP Referral Commision`);
-
-    // Percentages for each level
-    const percentages = [20, 2, 2, 2, 1.2, 1.2, 0.8, 0.8, 0.4, 0.4, 0.2, 0.2, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1];
-    console.log(percentages.length);
-
-    let currentMember = member.referredBy;
-    let level = 0;
-
-    const visited = new Set();
-
-    try {
-        while (currentMember && level < 20) {
-            if (visited.has(currentMember)) {
-                console.log('Cycle detected in referral chain. Breaking loop.');
-                break;
-            }
-            visited.add(currentMember);
-
-            // Find the upline member
-            const uplineMember = await Member.findOne({referralCode: currentMember}).select('_id fullName referralCode referredBy type');
-            if (!uplineMember)
-                break;
-
-            // Calculate the commission for this level
-            const percentage = percentages[level] ?? 0; // If percentage not specied, then 0 commission
-            const commission = (amount * percentage) / 100;
-
-
-            if (uplineMember.type !== 'VIP' && level < 3) {
-                console.log(`Upline Member ${uplineMember.fullName} (Level ${level + 1}) missed on receiving ${percentage}% (RM ${(commission / 100).toFixed(2)})`);
-            } else {
-                const uplineWallet = await Wallet.findOne({memberId: uplineMember._id}).select('balance');
-                if (!uplineWallet) {
-                    console.log(`Wallet not found for upline member ${uplineMember.fullName}`);
-                } else {
-                    uplineWallet.balance = Number(uplineWallet.balance) + commission;
-                    await uplineWallet.save();
-
-                    await Transaction.create({
-                        walletId: uplineWallet._id,
-                        systemType: 'HubWallet',
-                        type: 'Credit',
-                        description: 'VIP Registration Commission',
-                        status: 'Success',
-                        memberId: member._id,
-                        amount: amount
-                    });
-
-                    console.log(`Upline Member ${uplineMember.fullName} (Level ${level + 1}) received ${percentage}% (RM ${(commission / 100).toFixed(2)})`);
-                }
-            }
-
-            // Move to the next upline
-            currentMember = uplineMember.referredBy;
-            level++;
-        }
-    } catch (error) {
-        console.error(`Error processing VIP commission at Level ${level + 1}: ${error.message}`);
-        console.error(error.stack);
-    }
-};
 
 module.exports = {getPackage, purchasePackage, processVIPCommision};
