5ed52aba2fe7dba0dfa0498628353706a5a4933a
Faizal Ismail <muhammad.faizal@finexusgroup.com>
2025-01-03 03:46:29 +0800

    add helmet, compression, and increase json to 10mb

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const crypto = require('crypto');
 const cors = require('cors');
 const morgan = require('morgan');
 const express = require('express');
+const compression = require('compression');
+const helmet = require('helmet');
 const colors = require('colors');
 //const {v4: uuidv4} = require('uuid');
 
@@ -28,8 +30,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+
+app.use(helmet()); // apply security headers
+app.use(compression()); // apply response compression
 
 app.use((req, res, next) => {
     req.requestId = crypto.randomBytes(4).toString('hex').toUpperCase(); // 8-character ID
