b5f90a30d51b1f259cccd758b5a0b6ce739ae829
Faizal Ismail <muhammad.faizal@finexusgroup.com>
2025-02-23 03:52:12 +0800

    fix biometric login schema

--- routes/charityRoutes.js
+++ routes/charityRoutes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const {protect} = require('../middleware/authMiddleware');
 
 const {
-    getCharity
+    getCharity,
+    getCharityGallery
 } = require('../controllers/charityController');
 
 const router = express.Router();
 
 router.get('/', protect, getCharity);
+router.get('/gallery', protect, getCharityGallery);
 
 module.exports = router;
